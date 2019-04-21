f39f2fe88bf361a586c52c8c5739cb05c0efff7a
Utkarsh Chauhan <utkarsh.chauhan@hotmail.com>
2019-04-22 00:04:09 +0530

    Added some additional files and some bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FilesService } from './_services/files.services';
+import { MessageService } from './_services/message.service';
 import { StorageService } from './_services/storage.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +40,9 @@ import { SharedModule } from './shared/shared.module';
     MatIconModule
   ],
   providers: [
-    StorageService
+    StorageService,
+    MessageService,
+    FilesService
   ],
   bootstrap: [AppComponent]
 })
